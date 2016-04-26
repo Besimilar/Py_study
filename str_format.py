a5044c2b6e230acbd8514e667a5123e22127375b
Besimilar <besimilar@gmail.com>
2016-04-26 18:53:04 -0400

    create str_format.py

---
+++ str_format.py
@@ -0,0 +1,16 @@
+age = 20
+name = 'Swaroop'
+
+print('{0} was {1} years old when he wrote this book'.format(name,age))
+print('Why is {0} playing with that python?'.format(name))
+print(name + ' is ' + str(age) + ' years old')
+
+print('{} was {} years old when he wrote this book'.format(name,age))
+print('Why is {} playing with that python?'.format(name))
+
+print('a', end='')
+print('b')
+
+print('a', end=' ')
+print('b', end=' ')
+print('c')
